c3762f7c70d6aa40e0eaf793ace305f68b9cb55d
SURIYA <53847084+ssuriya1@users.noreply.github.com>
2024-06-18 23:55:18 +0530

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, () => {
+  console.log('Server running on port 3000');
+});
